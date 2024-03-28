9af95c5a02dbff5000e779ba982a8a4e7e5d364a
efrizal-m <efrizalsy0424@gmail.com>
2024-03-29 03:55:45 +0700

    init migrations and seeders

---
+++ migrations/index.js
@@ -0,0 +1,75 @@
+const pool = require('./config/connection')
+
+const usersQuery =
+`
+CREATE TABLE "Users" (
+    "_id" SERIAL PRIMARY KEY,
+    "email" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(50) NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "profile_image" VARCHAR(100)
+);`
+
+
+const bannersQuery =
+`
+CREATE TABLE "Banners" (
+    "_id" SERIAL PRIMARY KEY,
+    "banner_name" VARCHAR(30) NOT NULL,
+    "banner_image" VARCHAR(60) NOT NULL,
+    "description" VARCHAR(30) NOT NULL
+);`
+
+
+const servicesQuery =
+`
+CREATE TABLE "Services" (
+    "_id" SERIAL PRIMARY KEY,
+    "service_code" VARCHAR(30) NOT NULL,
+    "service_name" VARCHAR(30) NOT NULL,
+    "service_icon" VARCHAR(60) NOT NULL,
+    "service_tariff" INTEGER NOT NULL
+);`
+
+
+const balancesQuery =
+`
+CREATE TABLE "Balances" (
+    "_id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "balance_amount" INTEGER NOT NULL,
+    "created_on" DATE NOT NULL,
+    "updated_on" DATE NOT NULL
+);`
+
+
+const transactionsQuery =
+`
+CREATE TABLE "Transactions" (
+    "_id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "service_code" VARCHAR(30) NOT NULL,
+    "service_name" VARCHAR(30) NOT NULL,
+    "transaction_type" VARCHAR(30) NOT NULL,
+    "total_amount" INTEGER NOT NULL,
+    "created_on" DATE NOT NULL,
+    "updated_on" DATE NOT NULL
+);`
+
+
+pool.query(usersQuery)
+    .then(_ => { console.log('create users table succes')})
+    .catch(err => { console.log(err) })
+pool.query(bannersQuery)
+    .then(_ => { console.log('create banners table succes')})
+    .catch(err => { console.log(err) })
+pool.query(servicesQuery)
+    .then(_ => { console.log('create services table succes')})
+    .catch(err => { console.log(err) })    
+pool.query(balancesQuery)
+    .then(_ => { console.log('create balances table succes')})
+    .catch(err => { console.log(err) })    
+pool.query(transactionsQuery)
+    .then(_ => { console.log('create transactions table succes')})
+    .catch(err => { console.log(err) })    
---
+++ seeders/index.js
@@ -0,0 +1,34 @@
+const pool = require('./config/connection');
+const fs = require('fs').promises
+
+// read from local json
+const banners = JSON.parse(fs.readFileSync('./data/banners.json', 'utf8'))
+const services = JSON.parse(fs.readFileSync('./data/services.json', 'utf8'))
+
+// create array to be inputted as insertMany in next query string statement
+let arrBanners = []
+for (let i = 0; i < banners.length; i++) {
+    arrBanners.push(`('${banners[i].banner_name}','${banners[i].banner_image}','${banners[i].description}')`)
+}
+let arrServices = []
+for (let i = 0; i < services.length; i++) {
+    arrServices.push(`('${services[i].service_code}','${services[i].service_name}','${services[i].service_icon}','${services[i].service_tariff}',)`)
+}
+
+const InsertBanners = 
+`
+INSERT INTO "Banners" ("banner_name","banner_image","description")
+VALUES ${arrBanners.join(',')};
+`
+const InsertServices = 
+`
+INSERT INTO "Services" ("service_code","service_name","service_icon","service_tariff")
+VALUES ${arrServices.join(',')};
+`
+
+pool.query(InsertBanners)
+    .then(_ => { console.log('insert data banners succes')})
+    .catch(err => { console.log(err) })
+pool.query(InsertServices)
+    .then(_ => { console.log('insert data services succes')})
+    .catch(err => { console.log(err) })
